b19b72fb2efffe4b7ccd790577e67ba8f8ed0ff5
rifat71 <rifatmahmud24311@gmail.com>
2023-12-31 17:41:28 +0600

    add config and other files

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,30 @@
+describe('Login Test', () => {
+  it('should log in with valid credentials', () => {
+    cy.visit('/'); // Assumes your login page is at the root path
+
+    // Fill in the login form
+    cy.get('#username').type('your-username');
+    cy.get('#password').type('your-password');
+
+    // Submit the form
+    cy.get('form').submit();
+
+    // Add assertions based on your application behavior
+    // For example, check if the user is redirected to a dashboard page
+    // cy.url().should('include', '/dashboard');
+  });
+
+  it('should display an error message with invalid credentials', () => {
+    cy.visit('/');
+
+    // Fill in the login form with invalid credentials
+    cy.get('#username').type('invalid-username');
+    cy.get('#password').type('invalid-password');
+
+    cy.get('form').submit();
+
+    // Add assertions based on your application behavior
+    // For example, check if an error message is displayed
+    // cy.get('.error-message').should('be.visible');
+  });
+});
