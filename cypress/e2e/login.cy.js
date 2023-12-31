6b43e796444f4201d6ec86053dde8aac283ca1d7
rifat71 <rifatmahmud24311@gmail.com>
2024-01-01 03:47:12 +0600

    add invalid test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,24 +9,26 @@ describe('Login Test', () => {
     cy.get('#username', { timeout: 5000 }).type('galib');  // Exapmle to override default timeout
     cy.get('#password').type('123456');
 
-    // Submit the form
-    cy.get('form').submit();
+    // Click the login button
+    cy.get('#login_button').click();
 
     // Assertion check for successfull login
     cy.url().should('include', '/homey.html');
   });
 
-  //   it('log in with invalid credentials', () => {
-  //     cy.visit('/');
 
-  //     // Fill in the login form with invalid credentials
-  //     cy.get('#username').type('invalid-username');
-  //     cy.get('#password').type('invalid-password');
+  it('log in with invalid credentials', () => {
+    // Visit the link
+    cy.visit('http://127.0.0.1:5500/public/login.html');
 
-  //     cy.get('form').submit();
+    // Input credentials
+    cy.get('#username', { timeout: 5000 }).type('rifat');
+    cy.get('#password').type('12345');
 
-  //     // Add assertions based on your application behavior
-  //     // For example, check if an error message is displayed
-  //     // cy.get('.error-message').should('be.visible');
-  //   });
+    // Click the login button
+    cy.get('#login_button').click();
+
+    // Assertion check for successfull login
+    cy.url().should('include', '/homey.html');
+  });
 });
